fa4e11cf864219c503ba52df08b41585dd619627
Nika Perfilova <ees2009.2014@gmail.com>
2022-02-21 16:08:10 +0700

    feat: add a scoreboard and count moves

--- tic-tac-toe/js/index.js
+++ tic-tac-toe/js/index.js
@@ -14,13 +14,40 @@ const WINNING_COMBS = [
 
 const gameState = {
   turn: X,
+  movesCount: 0,
+  results: [],
 };
 
 const board = document.querySelector('.board');
 const cells = board.querySelectorAll('[data-cell]');
 const endMessage = document.querySelector('.end-message');
 const endMessageText = endMessage.querySelector('.end-message__text');
+const endMessageMovesTotal = endMessage.querySelector('.end-message__moves-total');
 const restartBtns = document.querySelectorAll('.restart-btn');
+const scoreboard = document.querySelector('.scoreboard');
+const scoreboardXWins = scoreboard.querySelector('.x-wins-count');
+const scoreboardOWins = scoreboard.querySelector('.o-wins-count');
+const scoreboardDraws = scoreboard.querySelector('.draws-count');
+
+const setLocalStorage = () => {
+  const { results } = gameState;
+  localStorage.setItem('results', JSON.stringify(results.slice(-10)));
+};
+
+const getLocalStorage = () => {
+  const results = localStorage.getItem('results');
+  gameState.results = results ? JSON.parse(results) : [];
+};
+
+const updateScoreboard = () => {
+  const { results } = gameState;
+  const xWinsCount = results.filter((res) => res === X).length;
+  const oWinsCount = results.filter((res) => res === CIRCLE).length;
+  const drawsCount = results.filter((res) => res === 'draw').length;
+  scoreboardXWins.textContent = xWinsCount;
+  scoreboardOWins.textContent = oWinsCount;
+  scoreboardDraws.textContent = drawsCount;
+};
 
 const switchTurns = () => {
   gameState.turn = gameState.turn === X ? CIRCLE : X;
@@ -37,8 +64,15 @@ const checkWin = () =>
 const checkDraw = () =>
   [...cells].every((cell) => cell.classList.contains(X) || cell.classList.contains(CIRCLE));
 
+const countResult = (currentResult) => {
+  gameState.results = [...gameState.results, currentResult].slice(-10);
+};
+
 const showEndMessage = (isDraw) => {
   endMessage.classList.add('end-message--active');
+  const { movesCount } = gameState;
+
+  endMessageMovesTotal.textContent = `Game ended in ${movesCount} step${movesCount < 2 ? '' : 's'}`;
 
   if (isDraw) {
     endMessageText.textContent = 'Draw!';
@@ -52,18 +86,26 @@ const handleCellClick = (e) => {
   const cell = e.target;
   cell.classList.add(gameState.turn);
 
+  gameState.movesCount += 1;
+
   if (checkWin()) {
+    countResult(gameState.turn);
+    updateScoreboard();
     showEndMessage(false);
     return;
   }
   if (checkDraw()) {
+    countResult('draw');
+    updateScoreboard();
     showEndMessage(true);
     return;
   }
+
   switchTurns();
 };
 
 const startGame = () => {
+  gameState.movesCount = 0;
   gameState.turn = X;
 
   board.classList.remove(X, CIRCLE);
@@ -78,6 +120,11 @@ const handleRestart = () => {
   startGame();
 };
 
-startGame();
+window.addEventListener('load', () => {
+  getLocalStorage();
+  updateScoreboard();
+  startGame();
+});
 
+window.addEventListener('beforeunload', setLocalStorage);
 restartBtns.forEach((btn) => btn.addEventListener('click', handleRestart));
