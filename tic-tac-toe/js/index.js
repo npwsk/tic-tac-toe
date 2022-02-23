56039aaadd3033a83b6252b4c97fb426d01846fe
Nika Perfilova <ees2009.2014@gmail.com>
2022-02-23 21:52:32 +0700

    feat: add new mode for playing against a computer

--- tic-tac-toe/js/index.js
+++ tic-tac-toe/js/index.js
@@ -1,5 +1,7 @@
 const X = 'x';
 const CIRCLE = 'circle';
+const MODE_HUMAN = 'human';
+const MODE_COMPUTER = 'computer';
 
 const WINNING_COMBS = [
   [0, 1, 2],
@@ -16,10 +18,15 @@ const gameState = {
   turn: X,
   movesCount: 0,
   results: [],
+  board: [...new Array(9)],
+  mode: {
+    name: MODE_COMPUTER,
+    computerClass: X,
+  },
 };
 
-const board = document.querySelector('.board');
-const cells = board.querySelectorAll('[data-cell]');
+const boardElem = document.querySelector('.board');
+const cells = [...boardElem.querySelectorAll('[data-cell]')];
 const endMessage = document.querySelector('.end-message');
 const endMessageText = endMessage.querySelector('.end-message__text');
 const endMessageMovesTotal = endMessage.querySelector('.end-message__moves-total');
@@ -29,6 +36,9 @@ const scoreboardXWins = scoreboard.querySelector('.x-wins-count');
 const scoreboardOWins = scoreboard.querySelector('.o-wins-count');
 const scoreboardDraws = scoreboard.querySelector('.draws-count');
 const currentTurnIcon = document.querySelector('.current-turn__turn-icon');
+const settingsBtn = document.querySelector('.settings-btn');
+const settingsModal = document.querySelector('.settings-modal');
+const modeCheckbox = settingsModal.querySelector('.mode-switch__checkbox');
 
 const setLocalStorage = () => {
   const { results } = gameState;
@@ -38,6 +48,10 @@ const setLocalStorage = () => {
 const getLocalStorage = () => {
   const results = localStorage.getItem('results');
   gameState.results = results ? JSON.parse(results) : [];
+};
+
+const setInitMode = () => {
+  modeCheckbox.checked = true;
 };
 
 const updateScoreboard = () => {
@@ -50,34 +64,8 @@ const updateScoreboard = () => {
   scoreboardDraws.textContent = drawsCount;
 };
 
-const updateTurn = () => {
-  currentTurnIcon.classList.remove(X, CIRCLE);
-  currentTurnIcon.classList.add(gameState.turn);
-};
-
-const switchTurns = () => {
-  gameState.turn = gameState.turn === X ? CIRCLE : X;
-
-  board.classList.remove(X, CIRCLE);
-  board.classList.add(gameState.turn);
-
-  updateTurn();
-};
-
-const checkWin = () =>
-  WINNING_COMBS.some((comb) =>
-    comb.every((index) => cells[index].classList.contains(gameState.turn))
-  );
-
-const checkDraw = () =>
-  [...cells].every((cell) => cell.classList.contains(X) || cell.classList.contains(CIRCLE));
-
-const countResult = (currentResult) => {
-  gameState.results = [...gameState.results, currentResult].slice(-10);
-};
-
 const showEndMessage = (isDraw) => {
-  endMessage.classList.add('end-message--active');
+  endMessage.classList.add('modal--active');
   const { movesCount } = gameState;
 
   endMessageText.classList.remove(X, CIRCLE);
@@ -94,51 +82,221 @@ const showEndMessage = (isDraw) => {
   endMessageText.innerHTML = `wins!`;
 };
 
-const handleCellClick = (e) => {
-  const cell = e.target;
-  cell.classList.add(gameState.turn);
+const hideEndMessage = (e) => {
+  if (!e.target.closest('.modal__inner')) {
+    endMessage.classList.remove('modal--active');
+  }
+};
 
-  gameState.movesCount += 1;
+const showSettingsModal = () => {
+  settingsModal.classList.add('modal--active');
+};
 
-  if (checkWin()) {
+const hideSettingsModal = (e) => {
+  if (!e.target.closest('.modal__inner')) {
+    settingsModal.classList.remove('modal--active');
+  }
+};
+
+const updateTurn = () => {
+  currentTurnIcon.classList.remove(X, CIRCLE);
+  currentTurnIcon.classList.add(gameState.turn);
+};
+
+const switchTurns = () => {
+  gameState.turn = gameState.turn === X ? CIRCLE : X;
+
+  boardElem.classList.remove(X, CIRCLE);
+  boardElem.classList.add(gameState.turn);
+
+  updateTurn();
+};
+
+const checkWin = (board, player) =>
+  WINNING_COMBS.some((comb) => comb.every((index) => board[index] === player));
+
+const getEmptyCellsIndexes = (board) =>
+  board.map((_el, index) => index).filter((index) => !board[index]);
+
+const checkDraw = (board) => getEmptyCellsIndexes(board).length === 0;
+
+const countResult = (currentResult) => {
+  gameState.results = [...gameState.results, currentResult].slice(-10);
+};
+
+const minimax = (board, isMaximizer) => {
+  const boardCopy = board.slice();
+  const player = isMaximizer ? X : CIRCLE;
+
+  if (checkWin(boardCopy, X)) {
+    return { score: 10 };
+  }
+  if (checkWin(boardCopy, CIRCLE)) {
+    return { score: -10 };
+  }
+  if (checkDraw(boardCopy)) {
+    return { score: 0 };
+  }
+
+  const availableSpots = getEmptyCellsIndexes(boardCopy);
+  const moves = [];
+
+  availableSpots.forEach((index) => {
+    const move = { index };
+    boardCopy[index] = player;
+    const { score } = minimax(boardCopy, !isMaximizer);
+    move.score = score;
+    boardCopy[index] = null;
+    moves.push(move);
+  });
+
+  let bestMove = { index: -1 };
+
+  if (isMaximizer) {
+    let bestScore = -1000;
+    moves.forEach((move) => {
+      if (move.score > bestScore) {
+        bestScore = move.score;
+        bestMove = move;
+      }
+    });
+  } else {
+    let bestScore = 1000;
+    moves.forEach((move) => {
+      if (move.score < bestScore) {
+        bestScore = move.score;
+        bestMove = move;
+      }
+    });
+  }
+
+  return bestMove;
+};
+
+const findBestMove = (board, player) => {
+  const bestMove = minimax(board, player === X);
+  return bestMove.index;
+};
+
+const checkGameOver = () => checkWin(gameState.board, gameState.turn) || checkDraw(gameState.board);
+
+const finishGame = () => {
+  if (checkWin(gameState.board, gameState.turn)) {
     countResult(gameState.turn);
     updateScoreboard();
     showEndMessage(false);
     return;
   }
-  if (checkDraw()) {
+  if (checkDraw(gameState.board)) {
     countResult('draw');
     updateScoreboard();
     showEndMessage(true);
-    return;
   }
+};
 
-  switchTurns();
+const makeMove = (player, index) => {
+  gameState.board[index] = player;
+  cells[index].classList.add(gameState.turn);
+
+  gameState.movesCount += 1;
+
+  const isGameOver = checkGameOver();
+
+  return isGameOver;
+};
+
+const makeBestMove = () => {
+  const bestMoveIndex = findBestMove(gameState.board, gameState.turn);
+  const isGameOver = makeMove(gameState.turn, bestMoveIndex);
+
+  return isGameOver;
+};
+
+const handleCellClick = (e) => {
+  const cell = e.target;
+  const cellIndex = cells.indexOf(cell);
+  switch (gameState.mode.name) {
+    case MODE_HUMAN: {
+      const isGameOver = makeMove(gameState.turn, cellIndex);
+
+      if (isGameOver) {
+        finishGame();
+        return;
+      }
+      switchTurns();
+      return;
+    }
+    case MODE_COMPUTER: {
+      let isGameOver = makeMove(gameState.turn, cellIndex);
+      if (isGameOver) {
+        finishGame();
+        return;
+      }
+      switchTurns();
+
+      const bestMoveIndex = findBestMove(gameState.board, gameState.turn);
+      cells[bestMoveIndex].removeEventListener('click', handleCellClick);
+
+      isGameOver = makeMove(gameState.turn, bestMoveIndex);
+
+      if (isGameOver) {
+        finishGame();
+        return;
+      }
+      switchTurns();
+      return;
+    }
+    default:
+      throw new Error(`Unknown mode name: ${gameState.mode.name}`);
+  }
 };
 
 const startGame = () => {
+  gameState.board = [...new Array(9)];
   gameState.movesCount = 0;
   gameState.turn = X;
 
   updateTurn();
 
-  board.classList.remove(X, CIRCLE);
-  board.classList.add(X);
+  boardElem.classList.remove(X, CIRCLE);
+  boardElem.classList.add(X);
 
-  cells.forEach((cell) => cell.addEventListener('click', handleCellClick, { once: true }));
+  if (gameState.mode.name === MODE_COMPUTER && gameState.mode.computerClass === X) {
+    makeBestMove();
+    switchTurns();
+  }
+
+  getEmptyCellsIndexes(gameState.board).forEach((index) =>
+    cells[index].addEventListener('click', handleCellClick, { once: true })
+  );
+};
+
+const restartGame = () => {
+  cells.forEach((cell) => cell.classList.remove(X, CIRCLE));
+  startGame();
 };
 
 const handleRestart = () => {
-  endMessage.classList.remove('end-message--active');
-  cells.forEach((cell) => cell.classList.remove(X, CIRCLE));
-  startGame();
+  endMessage.classList.remove('modal--active');
+  restartGame();
+};
+
+const handleModeInput = (e) => {
+  const checkbox = e.target;
+  gameState.mode.name = checkbox.checked ? MODE_COMPUTER : MODE_HUMAN;
+  restartGame();
 };
 
 window.addEventListener('load', () => {
   getLocalStorage();
   updateScoreboard();
+  setInitMode();
   startGame();
 });
 
 window.addEventListener('beforeunload', setLocalStorage);
 restartBtns.forEach((btn) => btn.addEventListener('click', handleRestart));
+settingsBtn.addEventListener('click', showSettingsModal);
+settingsModal.addEventListener('click', hideSettingsModal);
+endMessage.addEventListener('click', hideEndMessage);
+modeCheckbox.addEventListener('input', handleModeInput);
